428acec652e33d34667bbc4b9f875fd06a3b8df9
suzhongde <2373544536@qq.com>
2022-04-22 15:26:50 +0800

    feat: add user login

--- miniprogram/api/request.ts
+++ miniprogram/api/request.ts
@@ -1,4 +1,7 @@
-const baseUrl = 'https://yili-music-1662738-1309887746.ap-shanghai.run.tcloudbase.com'
+import { getToken, removeToken, setToken } from "../utils/auth"
+
+// const baseUrl = 'https://yili-music-1662738-1309887746.ap-shanghai.run.tcloudbase.com'
+const baseUrl = 'http://localhost:8080'
 
 
 
@@ -12,6 +15,20 @@ export const get = (uri: string) => {
         url: baseUrl + uri,
         method: 'GET',
         success: (res)=>{
+          if(res.statusCode === 401) {
+            removeToken()
+            const currentPages = getCurrentPages()
+            const currentRoute = currentPages[currentPages.length - 1].route
+            if (currentRoute !== 'pages/login/index') {
+              wx.navigateTo({
+                url: `/pages/login/index`
+              })
+            }
+            wx.showToast({
+              title: '用户未登录',
+              icon: 'error'
+            })
+          }
           resolve(res.data)
         },
         fail: reject,
@@ -20,4 +37,54 @@ export const get = (uri: string) => {
         }
       })
     })
+}
+
+export const post = (uri: string, data: object) => {
+  wx.showLoading({
+    title: '加载中'
+  })
+  
+  return new Promise<any>((resolve, reject)=>{
+    wx.request({
+      url: baseUrl + uri,
+      method: 'POST',
+      data,
+      success: (res)=>{
+        if(res.statusCode === 401) {
+          removeToken()
+          const currentPages = getCurrentPages()
+          const currentRoute = currentPages[currentPages.length - 1].route
+          if ( currentRoute !== 'page/login/index') {
+            wx.navigateTo({
+              url: `/pages/login/index`
+            })
+          }
+          wx.showToast({
+            title: '用户未登录',
+            icon: 'error'
+          })
+        }
+
+        _handleToken(res.header)
+
+        resolve(res.data)
+      },
+      fail: reject,
+      complete: ()=>{
+        wx.hideLoading()
+      }
+    })
+  })
+}
+
+
+const _handleToken = (header: any) => {
+  // 微信调用远程服务端authorization需要首字母小写-问题为解决
+  // const token = header['authorization'] || null
+  // 本地服务器(localhost)-Authorization
+  const token = header['Authorization'] || null
+  if (token && getToken() !== token) {
+    setToken(token)
+    wx.navigateBack()
+  }
 }
